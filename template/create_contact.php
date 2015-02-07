03c5910e9c679c8b8ba260fb3078a5d0ced6d415
Leonardo Faria <leonardofaria94@gmail.com>
2015-02-07 01:16:11 +0000

    Signed-off-by: Leonardo Faria <leonardofaria94@gmail.com>

--- template/create_contact.php
+++ template/create_contact.php
@@ -1,40 +1,40 @@
 
-	<div class="row" id="main-content">
+<div class="row" id="main-content">
 
-		<div class="col-md-6" id="sidebar">
+	<div class="col-md-6" id="sidebar">
 
-			<div id="contacts" class="well" id="contacts">
+		<div id="contacts" class="well" id="contacts">
 
-				<form action="actions/action_addContact.php" method="post">
-					<fieldset>
-						<div class="form-group col-md-10">
-							<div class="row">
-								<label>Nome </label> </br>
-								<input id="name" class="form-control" name="name" placeholder="Nome" type="text" autofocus>
-								</br> <label>Email</label> </br>
-								<input id="email" class="form-control" name="email" placeholder="Email" type="email" autofocus>
-								</br> <label>Telem�vel</label> </br>
-								<input id="phone" class="form-control" name="phone" placeholder="Telem�vel" type="tel" autofocus>
-							</div>
-							<div class="row col-md-10">
-								</br> <label>Observa��es</label> </br>
-								<textarea class="col-md-10" name="details" id="message"></textarea>
-							</div>
+			<form action="actions/action_addContact.php" method="post">
+				<fieldset>
+					<div class="form-group col-md-10">
+						<div class="row">
+							<label>Nome </label> </br>
+							<input id="name" class="form-control" name="name" placeholder="Nome" type="text" autofocus>
+							</br> <label>Email</label> </br>
+							<input id="email" class="form-control" name="email" placeholder="Email" type="email" autofocus>
+							</br> <label>Telem�vel</label> </br>
+							<input id="phone" class="form-control" name="phone" placeholder="Telem�vel" type="tel" autofocus>
+						</div>
+						<div class="row col-md-10">
+							</br> <label>Observa��es</label> </br>
+							<textarea class="col-md-10" name="details" id="message"></textarea>
+						</div>
+						</br>
+						<div class="row col-md-12 pull-right">
+							<!--LOGIN AND REGISTER BTN-->
 							</br>
-							<div class="row col-md-12 pull-right">
-								<!--LOGIN AND REGISTER BTN-->
-								</br>
-								<input name="submit" type="submit" class="btn btn-primary pull-right" value="Pronto!">
-							</div>
-
+							<input name="submit" type="submit" class="btn btn-primary pull-right" value="Pronto!">
 						</div>
 
-					</fieldset>
-				</form>
-			</div>
+					</div>
+
+				</fieldset>
+			</form>
 		</div>
-	
 	</div>
+
+</div>
 </div>
 
 v>
